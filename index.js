47c1202894aee85e77d3b57e33a6f6e887695e6d
AfdanSyahputra <m.afdansyahputra@student.uir.ac.id>
2025-07-07 11:56:55 +0700

    fix: use in-memory db for Vercel

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const path = require("path");
+const fs = require("fs");
 
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+
+// Load db.json content in-memory
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json"), "utf-8"));
+const router = jsonServer.router(data); // gunakan object, bukan path file
+
 server.use("/api", router);
 
 module.exports = server;
